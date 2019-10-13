bc319e8be897597fe7978ca3d69ff0b899f4da2a
Aviad Levy <aviadlevy1@gmail.com>
2019-10-13 08:32:36 +0300

    fix `Status` object not subscriptable

--- tweet_helper.py
+++ tweet_helper.py
@@ -7,7 +7,7 @@ from constant import EMPTY_SYMBOL, PROGRESS_SYMBOL, PROGRESS_BAR_WIDTH
 def get_last_state(tweets):
     for tweet in tweets:
         match = re.match(r'^[%s%s]{%d}\s(\d+)' % (EMPTY_SYMBOL, PROGRESS_SYMBOL, PROGRESS_BAR_WIDTH) + "%$",
-                         tweet['text'])
+                         tweet.text)
         if match:
             return int(match.group(1))
 
@@ -18,4 +18,4 @@ def should_tweet(last_state, current_state):
 
 def is_holiday_tweeted_today(timeline, holiday):
     return len([t for t in timeline if
-                t['created_at'].date() == datetime.datetime.today().date() and t['text'] == "Happy %s!" % holiday]) > 0
+                t.created_at.date() == datetime.datetime.today().date() and t.text == "Happy %s!" % holiday]) > 0
