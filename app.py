d556a3029d6d7acbdd767b2c4143d48bf3093336
aviadlevy <aviadlevy1@gmail.com>
2021-12-25 19:32:07 +0200

    remove Flask

--- app.py
+++ app.py
@@ -3,21 +3,18 @@ import traceback
 
 import slack
 import tweepy as tweepy
-from flask import Flask, jsonify, request
 
 from constant import PROGRESS_BAR_WIDTH, PROGRESS_SYMBOL, EMPTY_SYMBOL
-from dates_helper import get_current_state, get_holiday
+from dates_helper import get_current_state
 from progress_bar import ProgressBar
-from tweet_helper import is_holiday_tweeted_today, should_tweet, get_last_state
+from tweet_helper import should_tweet, get_last_state
 
 sc = slack.WebClient(os.environ['SLACK_API_TOKEN'])
-app = Flask(__name__)
 
 CONSUMER_KEY = os.environ['CONSUMER_KEY']
 CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
 ACCESS_KEY = os.environ['ACCESS_KEY']
 ACCESS_SECRET = os.environ['ACCESS_SECRET']
-API_KEY = os.environ['API_KEY']
 
 
 def get_progress_bar(current_state):
@@ -32,11 +29,7 @@ def send_slack_alert(msg):
     )
 
 
-@app.route('/tweet')
 def tweet():
-    if not ("apikey" in request.args and request.args.get("apikey") == API_KEY):
-        send_slack_alert("invalid client")
-        return "Invalid Client", 401
     current_state = get_current_state()
 
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -48,21 +41,16 @@ def tweet():
         last_state = get_last_state(timeline)
     except Exception as e:
         send_slack_alert("exception: " + repr(e) + "\n" + traceback.format_exc())
-        return jsonify(success=False, tweeted=False), 500
+        return -1
     is_tweeted = False
     if should_tweet(last_state, current_state):
         api.update_status(status=get_progress_bar(current_state))
         is_tweeted = True
-    holiday, is_happy = get_holiday()
-    # if holiday and not is_holiday_tweeted_today(timeline, holiday):
-    #    if is_happy:
-    #        api.update_status(status="Happy %s!" % holiday)
-    #    else:
-    #        api.update_status(status="%s\nHave an easy fast" % holiday)
+    print("Finish running. tweeted? -> %s. last state -> %sø" % (is_tweeted, last_state))
     send_slack_alert(
-        "Finish running. tweeted? -> %s. last state -> %s holiday -> %s" % (is_tweeted, last_state, holiday))
-    return jsonify(success=True, tweeted=is_tweeted, last_state=last_state, holiday=holiday is not None)
+        "Finish running. tweeted? -> %s. last state -> %s" % (is_tweeted, last_state))
+    return 0
 
 
 if __name__ == '__main__':
-    app.run(os.environ['PORT'])
+    exit(tweet())
