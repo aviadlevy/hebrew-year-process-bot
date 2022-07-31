1a7b9de588c7f6aaa257a13ff2748044fcd3b812
aviadlevy <aviadlevy1@gmail.com>
2022-07-31 20:00:28 +0300

    upgrade to API v2

--- tweet_progress.py
+++ tweet_progress.py
@@ -1,20 +1,10 @@
-import os
 import traceback
 
-import slack
-import tweepy as tweepy
-
+from config import get_client, get_slack_client
 from constant import PROGRESS_BAR_WIDTH, PROGRESS_SYMBOL, EMPTY_SYMBOL
 from dates_helper import get_current_state
 from progress_bar import ProgressBar
 from tweet_helper import should_tweet, get_last_state
-
-sc = slack.WebClient(os.environ['SLACK_API_TOKEN'])
-
-CONSUMER_KEY = os.environ['CONSUMER_KEY']
-CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
-ACCESS_KEY = os.environ['ACCESS_KEY']
-ACCESS_SECRET = os.environ['ACCESS_SECRET']
 
 
 def get_progress_bar(current_state):
@@ -22,7 +12,7 @@ def get_progress_bar(current_state):
         current_state)
 
 
-def send_slack_alert(msg):
+def send_slack_alert(sc, msg):
     sc.chat_postMessage(
         channel="#hebrew-year-process",
         text=msg
@@ -32,23 +22,21 @@ def send_slack_alert(msg):
 def tweet():
     current_state = get_current_state()
 
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    client = get_client()
+    sc = get_slack_client()
 
-    api = tweepy.API(auth)
-    timeline = api.user_timeline()
+    timeline = client.get_home_timeline()
     try:
-        last_state = get_last_state(timeline)
+        last_state = get_last_state(timeline.data)
     except Exception as e:
-        send_slack_alert("exception: " + repr(e) + "\n" + traceback.format_exc())
+        send_slack_alert(sc, "exception: " + repr(e) + "\n" + traceback.format_exc())
         return -1
     is_tweeted = False
     if should_tweet(last_state, current_state):
-        api.update_status(status=get_progress_bar(current_state))
+        client.create_tweet(text=get_progress_bar(current_state))
         is_tweeted = True
     print("Finish running. tweeted? -> %s. last state -> %sø" % (is_tweeted, last_state))
-    send_slack_alert(
-        "Finish running. tweeted? -> %s. last state -> %s" % (is_tweeted, last_state))
+    send_slack_alert(sc, "Finish running. tweeted? -> %s. last state -> %s" % (is_tweeted, last_state))
     return 0
 
 
