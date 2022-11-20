1a7d64f250ed3370544e0306d6334c4d67737d4d
aviadlevy <aviadlevy1@gmail.com>
2022-11-20 15:53:29 +0200

    add toot to mastodon

--- src/config.py
+++ src/config.py
@@ -1,6 +1,9 @@
+import asyncio
+import functools
 import os
 
 import slack
+from mastodon import Mastodon
 from tweepy.asynchronous import AsyncClient
 
 from async_stream_client import _AsyncStreamingClient
@@ -14,14 +17,14 @@ ACCESS_KEY = os.environ['ACCESS_KEY']
 ACCESS_SECRET = os.environ['ACCESS_SECRET']
 
 
-def get_async_client():
+def get_async_twitter_client():
     return AsyncClient(access_token=ACCESS_KEY, access_token_secret=ACCESS_SECRET,
                        consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET, wait_on_rate_limit=True)
 
 
 def get_async_stream():
     return _AsyncStreamingClient(
-        async_client=get_async_client(),
+        async_client=get_async_twitter_client(),
         slack_client=get_async_slack_client(),
         user_id=USER_ID,
         bearer_token=BEARER_TOKEN,
@@ -32,3 +35,14 @@ def get_async_slack_client() -> slack.AsyncWebClient:
     return slack.AsyncWebClient(os.environ['SLACK_API_TOKEN'])
 
 
+def get_mastodon_client():
+    return Mastodon(access_token=os.environ['MASTODON_ACCESS_TOKEN'], api_base_url='https://botsin.space')
+
+
+def run_in_executor(f):
+    @functools.wraps(f)
+    def inner(*args, **kwargs):
+        loop = asyncio.get_running_loop()
+        return loop.run_in_executor(None, lambda: f(*args, **kwargs))
+
+    return inner
