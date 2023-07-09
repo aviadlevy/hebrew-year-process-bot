a62469e6029b2407d62d9298776815f3d3c6f074
aviadlevy <aviadlevy1@gmail.com>
2023-07-09 10:37:51 +0300

    fix tests

--- src/tweet_helper.py
+++ src/tweet_helper.py
@@ -1,4 +1,3 @@
-import datetime
 import re
 
 from constant import EMPTY_SYMBOL, PROGRESS_SYMBOL, PROGRESS_BAR_WIDTH
@@ -14,18 +13,13 @@ def get_last_state(toots):
     """
     for toot in toots:
         match = re.match(r'^<p>[%s%s]{%d}\s(\d+)' % (EMPTY_SYMBOL, PROGRESS_SYMBOL, PROGRESS_BAR_WIDTH) + "%</p>$",
-                         toot.content)
+                         toot["content"])
         if match:
             return int(match.group(1))
 
 
 def should_tweet(last_state, current_state):
     return current_state > last_state or (last_state == 100 and current_state == 0)
-
-
-def is_holiday_tweeted_today(timeline, holiday):
-    return len([t for t in timeline if
-                t.created_at.date() == datetime.datetime.today().date() and "%s" % holiday in t.text]) > 0
 
 
 def get_text_to_reply(text):
