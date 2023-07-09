f1d643879262a1bfb9123c67c831facca67d8895
aviadlevy <aviadlevy1@gmail.com>
2023-07-09 10:26:55 +0300

    Resolve #34

--- src/tweet_helper.py
+++ src/tweet_helper.py
@@ -5,10 +5,16 @@ from constant import EMPTY_SYMBOL, PROGRESS_SYMBOL, PROGRESS_BAR_WIDTH
 from lang import MESSAGES
 
 
-def get_last_state(tweets):
-    for tweet in tweets:
-        match = re.match(r'^[%s%s]{%d}\s(\d+)' % (EMPTY_SYMBOL, PROGRESS_SYMBOL, PROGRESS_BAR_WIDTH) + "%$",
-                         tweet.text)
+def get_last_state(toots):
+    """
+    extract the last state from Mastodon
+
+    :param toots: toots from Mastodon
+    :return:
+    """
+    for toot in toots:
+        match = re.match(r'^<p>[%s%s]{%d}\s(\d+)' % (EMPTY_SYMBOL, PROGRESS_SYMBOL, PROGRESS_BAR_WIDTH) + "%</p>$",
+                         toot.content)
         if match:
             return int(match.group(1))
 
